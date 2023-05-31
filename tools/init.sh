a77db0baa04faf78aced0cfbd4540c49d4c3f9ce
kawasaki kouji <7048018+bianchiidentity@users.noreply.github.com>
2023-05-31 18:15:08 +0900

    Add init.sh for nginx and mysql

---
+++ tools/init.sh
@@ -0,0 +1,21 @@
+#!/bin/bash
+
+cd /home/isucon || exit
+
+# nginx
+if [ ! -e "/home/isucon/etc/nginx/nginx.conf" ]; then
+  mkdir -p /home/isucon/etc/nginx/
+  sudo cp /etc/nginx/nginx.conf /home/isucon/etc/nginx/nginx.conf
+  git add /home/isucon/etc/nginx/nginx.conf
+  git commit -m "backup nginx.conf"
+  git push
+fi
+
+# mysql
+if [ ! -e "/home/isucon/etc/mysql/mysql.conf.d/mysqld.cnf" ]; then
+  mkdir -p /home/isucon/etc/mysql/mysql.conf.d
+  sudo cp /etc/mysql/mysql.conf.d/mysqld.cnf /home/isucon/etc/mysql/mysql.conf.d/mysqld.cnf
+  git add /home/isucon/etc/mysql/mysql.conf.d/mysqld.cnf
+  git commit -m "backup mysqld.cnf"
+  git push
+fi
