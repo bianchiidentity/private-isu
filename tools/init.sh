a945597c083c9a4d728f0b69bfad946a888e0e3a
kawasaki kouji <7048018+bianchiidentity@users.noreply.github.com>
2023-05-31 18:33:08 +0900

    forgot symbolic link

--- tools/init.sh
+++ tools/init.sh
@@ -6,6 +6,7 @@ cd /home/isucon || exit
 if [ ! -e "/home/isucon/etc/nginx/nginx.conf" ]; then
   mkdir -p /home/isucon/etc/nginx/
   sudo cp /etc/nginx/nginx.conf /home/isucon/etc/nginx/nginx.conf
+  ln -sf /home/isucon/etc/nginx/nginx.conf /etc/nginx/nginx.conf
   git add /home/isucon/etc/nginx/nginx.conf
   git commit -m "backup nginx.conf"
   git push
@@ -15,6 +16,7 @@ fi
 if [ ! -e "/home/isucon/etc/mysql/mysql.conf.d/mysqld.cnf" ]; then
   mkdir -p /home/isucon/etc/mysql/mysql.conf.d
   sudo cp /etc/mysql/mysql.conf.d/mysqld.cnf /home/isucon/etc/mysql/mysql.conf.d/mysqld.cnf
+  ln -sf /home/isucon/etc/mysql/mysql.conf.d/mysqld.cnf /etc/mysql/mysql.conf.d/mysqld.cnf
   git add /home/isucon/etc/mysql/mysql.conf.d/mysqld.cnf
   git commit -m "backup mysqld.cnf"
   git push
